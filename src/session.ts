9eac741896dd5bfc3d6d21bac0770f374e713e59
Charlie Thomson <charliecthomson@gmail.com>
2023-08-04 06:24:46 -0400

    cleanup + session -> localstorage

--- src/session.ts
+++ src/session.ts
@@ -1,9 +1,12 @@
 import { createSession, createShim, s } from "@charliethomson/better-session";
 import moment from "moment";
 
-export const session = createSession({
-  lastVoteTime: s.custom(
-    "vote-lasttime",
-    createShim(moment, (m) => m.toISOString())
-  ),
-});
+export const session = createSession(
+  {
+    lastVoteTime: s.custom(
+      "vote-lasttime",
+      createShim(moment, (m) => m.toISOString())
+    ),
+  },
+  { storage: localStorage, mode: "prod" }
+);
